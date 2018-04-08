e5a32547e5d215d20bdef802f47475940791e84f
sevap8 <34450118+sevap8@users.noreply.github.com>
2018-04-08 17:22:32 +0300

    Update App.java

--- lab3/App.java
+++ lab3/App.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.lab3;
 
-/**
- *
- * @author seva
- */
+import javax.xml.ws.Endpoint;
+
 public class App {
     
+
+    public static void main(String[] args) {
+    //disable stacktraces in soap-message
+   System.setProperty("com.sun.xml.ws.fault.SOAPFaultBuilder.disableCaptureStack Trace", "false");
+
+   String url = "http://0.0.0.0:8080/PersonService";
+
+    Endpoint.publish(url, new PersonWebService());
+    }
 }
+
+
