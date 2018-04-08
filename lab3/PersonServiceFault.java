b40851252c6383dc25bb0bdc3769ad1a566a3495
sevap8 <34450118+sevap8@users.noreply.github.com>
2018-04-08 17:26:12 +0300

    Update PersonServiceFault.java

--- lab3/PersonServiceFault.java
+++ lab3/PersonServiceFault.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.lab3;
 
-/**
- *
- * @author seva
- */
 public class PersonServiceFault {
-    
+    private static final String DEFAULT_MESSAGE = "Parameter q cannot be null
+or empty";
+    private String message;
+
+    public String getMessage() {
+    return message;
+    }
+    public void setMessage(String message) {
+    this.message = message;
+    }
+    public static PersonServiceFault defaultInstance() {
+        PersonServiceFault fault = new PersonServiceFault();
+        fault.message = DEFAULT_MESSAGE;
+        return fault;
+    }
 }
