61e7da464b1be0ab27192174d4df3b7d1203129f
sevap8 <34450118+sevap8@users.noreply.github.com>
2018-04-08 17:24:45 +0300

    Update InsertingException.java

--- lab3/InsertingException.java
+++ lab3/InsertingException.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.lab3;
 
-/**
- *
- * @author seva
- */
-public class InsertingException {
+
+import javax.xml.ws.WebFault;@WebFault(faultBean = "com.maxart.service.PersonServiceFault")
+public class InsertingException extends Exception {
+    private static final long serialVersionUID = -6647544772732631047L;
+    private final PersonServiceFault fault;
     
+    public InsertingException(String message, PersonServiceFault fault) {
+
+        super(message);
+        this.fault = fault;
+    }
+
+    public InsertingException(String message, PersonServiceFault fault,
+Throwable cause) {
+        super(message, cause);
+        this.fault = fault;
+    }
+       public PersonServiceFault getFaultInfo() {
+         return fault;
+    }
 }
