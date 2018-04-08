a4e091841dff7deced1a30d466e307f72512b768
sevap8 <34450118+sevap8@users.noreply.github.com>
2018-04-08 17:25:14 +0300

    Update InvalidCreatingParametersException.java

--- lab3/InvalidCreatingParametersException.java
+++ lab3/InvalidCreatingParametersException.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.mycompany.lab3;
 
-/**
- *
- * @author seva
- */
-public class InvalidCreatingParametersException {
+import javax.xml.ws.WebFault;
+
+@WebFault(faultBean = "com.maxart.service.PersonServiceFault")
+public class InvalidCreatingParametersException extends Exception {
+    private static final long serialVersionUID = -6647544772732631047L;
+    private final PersonServiceFault fault;
+
+    public InvalidCreatingParametersException(String message,
+PersonServiceFault fault) {
+    super(message);
+    this.fault = fault;
+    }
     
+    public InvalidCreatingParametersException(String message,
+PersonServiceFault fault, Throwable cause) {
+    super(message, cause);
+    this.fault = fault;
+    }
+    
+    public PersonServiceFault getFaultInfo() {
+        return fault;
+    }
 }
